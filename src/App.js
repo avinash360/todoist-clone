8ac71dd5d738d7f44231e986056ca27a6acfc0cd
Avinash Thakur <avithakur86@gmail.com>
2021-08-26 02:04:29 +0530

    add: layout code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import { Content } from './components/layout/content';
+import { Header } from './components/layout/Header';
 
-export const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
-}
+export const App = () => (
+  <div className="App">
+    <Header />
+    <Content />
+  </div>
+);
